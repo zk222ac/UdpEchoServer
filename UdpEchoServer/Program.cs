01cb840d14b9bc3413b6b0e8dec723a188fa222e
Zuhair Haroon Khan <zk222ac@gmail.com>
2018-10-17 00:15:50 +0200

    final relsease

--- UdpEchoServer/Program.cs
+++ UdpEchoServer/Program.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,12 +12,47 @@ namespace UdpEchoServer
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            // Create a UdpClient for reading incoming data 
+           UdpClient udpServerClient = new UdpClient(9999);
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            // Create an IPEndPoint to record the IP and port of sender
+            IPAddress senderIpAddress = IPAddress.Loopback;
+            // sender port
+            int senderPort = 9999;
+            IPEndPoint remoteIpEndPoint = new IPEndPoint(senderIpAddress, senderPort);
+
+            Console.WriteLine("Hello World! Here is UDP Server!");
+
+            try
+            {
+                // Block until a message is received from this socket  from a remote host ( client)
+                Console.WriteLine("Server is Blocked");
+                while (true)
+                {
+                    Byte[] receivedBytes = udpServerClient.Receive(ref remoteIpEndPoint);
+                    Console.WriteLine("Now server is activated");
+                    string receivedMsg = Encoding.ASCII.GetString(receivedBytes);
+                    Console.WriteLine($"Received Message from Client: {receivedMsg}");
+                    string[] data = receivedMsg.Split(' ');
+                    string clientName = data[0];
+                    string text = data[1] + data[2];
+
+                    // server send back data to client 
+                    string sendData = $"Server : {text}";
+                    Byte[] sendBytes = Encoding.ASCII.GetBytes(sendData);
+                    udpServerClient.Send(sendBytes, sendBytes.Length, remoteIpEndPoint);
+                    Console.WriteLine($"The Message was sent by IP:{remoteIpEndPoint.Address} on their Port :{remoteIpEndPoint.Port}");
+                   
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            
+
+           
+
         }
     }
 }
